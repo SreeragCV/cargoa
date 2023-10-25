e774349883f0391d869bb72adee7f38db24b8024
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-25 18:25:23 +0530

    Login and SignUp

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/cargoa')
 app.use(express.json())
 
 app.post('/create', user.registerUser)
+app.post('/login', user.loginUser)
 
 
 app.listen(3000, () => {
--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,30 @@ module.exports.registerUser = async (req, res) => {
   try {
     const { username, email, password, role } = req.body;
     const hashPassword = await bcrypt.hash(password, 12);
-    const user = new User({ username, email, hashPassword, role });
-    await user.save();
-    res.send(user);
-    console.log(user);
+    const userExist = await User.find({ email: email });
+    if (userExist.length === 0) {
+      const user = new User({ username, email, hashPassword, role });
+      await user.save();
+      res.send(user);
+    } else {
+      res.status(409).json("EMAIL ALREADY REGISTERED");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports.loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.find({ username: username });
+    const userPassword = user[0].hashPassword;
+    const validPassword = await bcrypt.compare(password, userPassword);
+    if (validPassword) {
+      res.send(user);
+    } else {
+      res.status(401).json("WRONG PASSWORD!!");
+    }
   } catch (err) {
     res.status(500).json(err);
   }
