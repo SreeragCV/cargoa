af12ce2c19a04a84a3393841e167d423a713abe8
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-26 16:08:10 +0530

    Create for Products

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,24 @@ const Product = require("../models/product");
 const User = require("../models/user");
 
 module.exports.CreateProduct = async (req, res) => {
-  const { name, quantity, shippingDate, vendorname } = req.body;
-  const vendor = await User.findOne({ username: vendorname });
-  if (vendor.role === "vendor") {
-  const product = new Product({ name, quantity, shippingDate });
-  product.vendorID = vendor
-  res.send(product)
-  } else {
-    res.status(400).json('CHOOSE A PROPER VENDOR');
+  try {
+    const { name, quantity, shippingDate, vendorname } = req.body;
+    const vendor = await User.findOne({ username: vendorname });
+    const currentUser = await User.findOne({ email: req.user.email });
+    if (vendor.role === "vendor") {
+      if (req.user.role === "user") {
+        const product = new Product({ name, quantity, shippingDate });
+        product.vendorID = vendor;
+        product.userID = currentUser;
+        res.send(product);
+        await product.save();
+      } else {
+        res.status(400).json("YOU ARE NOT A USER");
+      }
+    } else {
+      res.status(400).json("CHOOSE A PROPER VENDOR");
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
